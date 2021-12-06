911bfc7de7933cdaf0f1889675aecd7b800aa6ea
K.Popov <nataha_sergeii@mail.ru>
2021-12-07 02:19:08 +0300

    added ball throw and import models

---
+++ Assets/Objects/Ball/Scripts/Ball.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using Varwin.Public;
+
+namespace Varwin.Types.Ball
+{
+    [VarwinComponent(English: "Bowling ball", Russian: "Шар для боулинга")]
+    [RequireComponent(typeof(Rigidbody))]
+    public class Ball : VarwinObject, IGrabStartAware, IGrabEndAware
+    {
+        [Locale(SystemLanguage.English, "Power of the throw")]
+        [Locale(SystemLanguage.Russian, "Сила броска")]
+        [SerializeField] private float _throwForce;
+
+        private BallThrowLogic _ballThrowLogic;
+        private Rigidbody _rigidbody;
+
+        private void Awake()
+        {
+            _rigidbody = GetComponent<Rigidbody>();
+            _ballThrowLogic = new BallThrowLogic();
+        }
+
+        public void OnGrabStart(GrabingContext context)
+        {
+            _ballThrowLogic.GrabStart(context);
+        }
+
+        public void OnGrabEnd()
+        {
+            _ballThrowLogic.GrabEnd(_rigidbody, _throwForce);
+        }
+    }
+}
---
+++ Assets/Objects/Ball/Scripts/BallThrowLogic.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+namespace Varwin.Types.Ball
+{
+    public class BallThrowLogic
+    {
+        private Camera _playerCamera;
+        private Transform _viewDirection;
+        
+        public void GrabStart(GrabingContext context)
+        {
+            _viewDirection = context.GameObject.transform;
+        }
+
+        public void GrabEnd(Rigidbody rigidbody, float throwForce)
+        {
+            rigidbody.AddForce(_viewDirection.forward * throwForce);
+        }
+    }
+}
